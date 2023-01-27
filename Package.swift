89ccec03ab13e0bffc2f91883a05c5ea3a3d80ec
Lyuboslav Ivanov <luboslav.ivanov@azdigital.bg>
2023-01-27 11:42:42 +0200

    [UPDATE]: Package dependancies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
                     targets: ["ScanQrKit"]),
         ],
         dependencies: [
-            .package(url: "https://github.com/horizontalsystems/ComponentKit.Swift.git", .upToNextMajor(from: "1.0.0")),
-            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/LuboDev/ComponentKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/LuboDev/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
         ],
         targets: [
             .target(
